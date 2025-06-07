449c89400129b514aa8d6906328f1da87b0242cb
Endenissanse <dov.raven@gmail.com>
2025-06-08 01:20:06 +0300

    corrected func ConnectDB

--- pkg/database/database_connection.go
+++ pkg/database/database_connection.go
@@ -9,21 +9,21 @@ import (
 	"github.com/tarkour/product-service/pkg/config"
 )
 
-func ConnectDB() (*pgx.Conn, error) {
+func ConnectDB(path string) (*pgx.Conn, error) {
 
-	cfg, err := config.LoadConfig("./internal/config")
+	cfg, err := config.LoadConfig(path)
 	if err != nil {
 		log.Fatal("Config error: ", err)
 	}
 
 	conn, err := pgx.Connect(context.Background(), cfg.Database.GetConnectionString())
 	if err != nil {
-		return nil, fmt.Errorf("Database connection error: %v", err)
+		return nil, fmt.Errorf("database connection error: %v", err)
 	}
 
 	err = conn.Ping(context.Background())
 	if err != nil {
-		return nil, fmt.Errorf("Database test ping error: %v", err)
+		return nil, fmt.Errorf("database test ping error: %v", err)
 	}
 
 	return conn, nil
